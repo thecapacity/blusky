d68d4932344a519e11f3d842a03ca7ccfbaadf84
Vicki Boykis <vicki.boykis@gmail.com>
2024-11-15 21:24:35 -0500

    refactor api

--- get_posts.py
+++ get_posts.py
@@ -2,19 +2,15 @@ from atproto import Client, client_utils
 import os
 from dotenv import load_dotenv
 
-
-def main():
+def get_bsky_client():
     load_dotenv()
-    USER = os.getenv('BSKY_USER')
-    PWD = os.getenv('BSKY_PWD')
-    
     client = Client()
-    profile = client.login(USER,PWD)
-    print('Welcome,', profile.display_name)
-
-    followers = client.get_followers('vickiboykis.com')
-    print(followers)
+    client.login(os.getenv('BSKY_USER'), os.getenv('BSKY_PWD'))
+    return client
 
 
 if __name__ == '__main__':
-    main()
+    client = get_bsky_client()
+    print('Welcome,', client.me.display_name)
+    followers = client.get_followers('vickiboykis.com')
+    print(followers)
